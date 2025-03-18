4d202dbfd617d9ead357a1311516d8e5c09318f5
Peiyi Xu <pxu@dumac.duke.edu>
2025-03-18 18:53:13 -0400

    added web request with wait

---
+++ chapter04/web_request_with_wait.py
@@ -0,0 +1,24 @@
+import asyncio
+from aiohttp import ClientSession
+from util import async_timed
+from chapter04 import fetch_status
+
+
+@async_timed()
+async def main():
+    async with ClientSession() as session:
+        fetchers = [
+            asyncio.create_task(fetch_status(session, "https://www.google.com")),
+            asyncio.create_task(fetch_status(session, "https://www.google.com")),
+        ]
+        done, pending = await asyncio.wait(fetchers)
+
+        print(f'Done task count: {len(done)}')
+        print(f'Pending task count: {len(pending)}')
+
+        for done_task in done:
+            result = await done_task
+            print(result)
+
+
+asyncio.run(main())
