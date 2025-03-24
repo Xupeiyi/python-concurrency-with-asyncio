be69711ca954967b8e5ad0f6e7c346f75413b10e
Peiyi Xu <pxu@dumac.duke.edu>
2025-03-24 17:49:58 -0400

    feat: add script for inserting random brand names into the database

---
+++ chapter05/insert_random_brands.py
@@ -0,0 +1,34 @@
+import os
+import asyncio
+from random import sample
+import asyncpg
+
+
+def load_common_words() -> list[str]:
+    with open('common_words.txt') as f:
+        return f.readlines()
+
+
+def generate_brand_names(words: list[str]) -> list[tuple[str]]:
+    return [(words[index],) for index in sample(range(100), 100)]
+
+
+async def insert_brands(common_words, connection) -> int:
+    brands = generate_brand_names(common_words)
+    sql = "INSERT INTO brand VALUES(DEFAULT, $1)"
+    return await connection.executemany(sql, brands)
+
+
+async def main():
+    common_words = load_common_words()
+    connection = await asyncpg.connect(
+        host='127.0.0.1',
+        port=5432,
+        user='postgres',
+        database='products',
+        password=os.getenv('DB_PASSWORD')
+    )
+    await insert_brands(common_words, connection)
+
+
+asyncio.run(main())
