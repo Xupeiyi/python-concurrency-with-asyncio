b44417f672defbb608ef0d27f9b8d44e345b50ac
Peiyi Xu <xu_py311@outlook.com>
2025-08-07 22:22:53 -0400

    refactored async_context_manager.py

--- chapter04/async_context_manager.py
+++ chapter04/async_context_manager.py
@@ -6,39 +6,42 @@ from typing import Optional, Type
 
 class ConnectedSocket:
 
-    def __init__(self, server_socket):
-        self._connection = None
-        self._server_socket = server_socket
+    def __init__(self, listening_socket):
+        self._listening_socket = listening_socket
+        self._connected_socket = None
 
     async def __aenter__(self):
         print('Entering context manager, waiting for connection')
-        loop = asyncio.get_event_loop()
-        connection, address = await loop.sock_accept(self._server_socket)
-        self._connection = connection
+        loop = asyncio.get_running_loop()
+        connected_socket, address = await loop.sock_accept(self._listening_socket)
+        self._connected_socket = connected_socket
         print(f'Accepted connection from {address}')
-        return self._connection
+        return self._connected_socket
     
-    async def __aexit__(self, 
-                        exc_type: Optional[Type[BaseException]],
-                        exc_val: Optional[BaseException],
-                        exc_tb: Optional[TracebackType]):
+    async def __aexit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType]
+    ):
         print('Exiting context manager')
-        self._connection.close()
+        self._connected_socket.close()
         print('Closed connection')
     
 
 async def main():
-    loop = asyncio.get_event_loop()
-    server_socket = socket.socket()
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listening_socket = socket.socket()
+    listening_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_address = ('127.0.0.1', 8000)
-    server_socket.setblocking(False)
-    server_socket.bind(server_address)
-    server_socket.listen()
+    listening_socket.setblocking(False)
+    listening_socket.bind(server_address)
+    listening_socket.listen()
 
-    async with ConnectedSocket(server_socket) as connection:
-        data = await loop.sock_recv(connection, 1024)
+    async with ConnectedSocket(listening_socket) as connected_socket:
+        loop = asyncio.get_running_loop()
+        data = await loop.sock_recv(connected_socket, 1024)
         print(f"I got data: {data}")
 
 
-asyncio.run(main())
+if __name__ == '__main__':
+    asyncio.run(main())
