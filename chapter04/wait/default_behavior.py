c10af5f7b55d9b514ed16c6ecc34d3dcff9ac4f4
Peiyi Xu <xu_py311@outlook.com>
2025-08-09 10:40:14 -0400

    added comment in default_behavior.py

--- chapter04/wait/default_behavior.py
+++ chapter04/wait/default_behavior.py
@@ -11,12 +11,17 @@ async def main():
             asyncio.create_task(fetch_status(session, "https://www.google.com")),
             asyncio.create_task(fetch_status(session, "https://www.google.com")),
         ]
+
+        # by default, we are using return_when = asyncio.ALL_COMPLETED.
+        # the pending set will always be empty
         done, pending = await asyncio.wait(fetchers)
 
         print(f'Done task count: {len(done)}')
         print(f'Pending task count: {len(pending)}')
 
         for done_task in done:
+            # if any task went wrong, the exception is raised here
+            # rather than at await asyncio.wait
             result = await done_task
             print(result)
 
