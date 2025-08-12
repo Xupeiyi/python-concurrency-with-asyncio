9a6932c8a36aaf4ba8b3b30f0415399b3f60417d
Peiyi Xu <xu_py311@outlook.com>
2025-08-11 22:05:41 -0400

    refactored clean_shutdown_with_thread_subclass.py

--- chapter07/clean_shutdown_with_thread_subclass.py
+++ chapter07/clean_shutdown_with_thread_subclass.py
@@ -4,44 +4,44 @@ from threading import Thread
 
 class ClientEchoThread(Thread):
 
-    def __init__(self, client):
+    def __init__(self, socket_):
         super().__init__()
-        self.client = client
+        self.connected_socket = socket_
 
     def run(self):
         try:
             while True:
-                data = self.client.recv(2048)
+                data = self.connected_socket.recv(2048)
 
-                # when connection was closed by the client or shut down
+                # when connection was closed by the client or shut down by ourselves
                 if not data:   
                     raise BrokenPipeError('Connection Closed!')
                 
                 print(f"Received: {data.decode()}, sending!")
-                self.client.sendall(data)
+                self.connected_socket.sendall(data)
 
         # thrown from sendall() when the client closed the connection
         except OSError as e:
-            print(f"Thread interrupted by {e} excpetion, shutting down!")
+            print(f"Thread interrupted by {e} exception, shutting down!")
         
     def close(self):
         # if the run() method is still running
         if self.is_alive():
-            self.client.sendall(bytes("Shutting down!", encoding="utf-8"))
+            self.connected_socket.sendall("Shutting down!\n".encode())
             # shutdown for read and write
-            self.client.shutdown(socket.SHUT_RDWR)
+            self.connected_socket.shutdown(socket.SHUT_RDWR)
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind(('127.0.0.1', 8000))
-    server.listen()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as listening_socket:
+    listening_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listening_socket.bind(('127.0.0.1', 8000))
+    listening_socket.listen()
     connection_threads = []
 
     try:
         while True:
-            connection, addr = server.accept()
-            thread = ClientEchoThread(connection)
+            connected_socket, addr = listening_socket.accept()
+            thread = ClientEchoThread(connected_socket)
             connection_threads.append(thread)
             thread.start()      
     except KeyboardInterrupt:
